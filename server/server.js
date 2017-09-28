edfb6c27e44385424b53eca956a076234079226c
JUSTIN-PC\Justin <justin1lisi@gmail.com>
2017-09-28 16:03:35 -0400

    Find by id added /GET

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,25 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send({error: 'ID is not valid'});
+  }
+
+  Todo.findById(id).then((todo) => {
+
+    if(!todo){
+      return res.status(404).send({error: 'Todo not found'});
+    }
+    
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
