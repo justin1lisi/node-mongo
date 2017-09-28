1fa6f4b8281d9da9501344be02e33a1f5270340c
JUSTIN-PC\Justin <justin1lisi@gmail.com>
2017-09-28 14:00:33 -0400

    added users mongoose

--- server/server.js
+++ server/server.js
@@ -5,23 +5,45 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 var newTodo = new Todo({
-  text: 'Cook dinner tonight',
-  completed: true,
-  completedAt: 5
+  text: 'Testing'
 });
 
-newTodo.save().then((doc) => {
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save Todo');
+// });
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: 'justin1lisi@gmail.com'
+});
+
+newUser.save().then((doc) => {
   console.log('Saved todo', doc);
 }, (e) => {
   console.log('Unable to save Todo');
